d5e53e4750fb041adaa49c63e232e37e56758212
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2025-07-22 21:03:19 -0500

    A way to perform insertions

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -15,16 +15,18 @@ export class SeedService {
   ) {}
 
   async executeSeed() {
+    await this.pokemonModel.deleteMany({});
     const { data } = await this.axios.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
-
+    const insertPromisesArray: Promise<any>[] = [];
     for (const { name, url } of data.results) {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
-      await this.pokemonModel.create({ name, no });
+      // await this.pokemonModel.create({ name, no });
+      insertPromisesArray.push(this.pokemonModel.create({ name, no }));
     }
-
+    await Promise.all(insertPromisesArray);
     return 'Seed executed successfully';
   }
 }
